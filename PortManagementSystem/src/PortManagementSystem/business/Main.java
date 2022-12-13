cabfc2a2fa2413e17ba0cf3bead14e686e2cd579
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2022-12-13 17:56:24 +0300

    main

--- PortManagementSystem/src/PortManagementSystem/business/Main.java
+++ PortManagementSystem/src/PortManagementSystem/business/Main.java
@@ -1,5 +1,5 @@
 package PortManagementSystem.business;
 
 public class Main {
-    
+
 }
--- PortManagementSystem/src/PortManagementSystem/entities/Concrete/Port.java
+++ PortManagementSystem/src/PortManagementSystem/entities/Concrete/Port.java
@@ -1,4 +1,36 @@
 package PortManagementSystem.entities.Concrete;
 
-public class Port {
+import PortManagementSystem.entities.Abstract.Container;
+import PortManagementSystem.entities.Abstract.IPort;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Port implements IPort {
+
+    private int ID;
+    private double X;
+    private double Y;
+
+    ArrayList<Container> containers;
+
+    ArrayList<Ship> history;
+
+    ArrayList<Ship> current;
+
+    @Override
+    public void incomingShip(Ship s) {
+
+    }
+
+    @Override
+    public void outgoingShip(Ship s) {
+
+    }
+
+    double getDistance(Port other)
+    {
+        // TODO calculates the distance between the object itself and another Port
+        return 0;
+    }
 }
