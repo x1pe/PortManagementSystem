f89253c03b22cd720a67f35d0981c7cddc0e5f49
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2022-12-17 15:40:36 +0300

    menu formatting

--- PortManagementSystem/src/PortManagementSystem/ConsoleUI/Main.java
+++ PortManagementSystem/src/PortManagementSystem/ConsoleUI/Main.java
@@ -1,7 +1,102 @@
 package PortManagementSystem.ConsoleUI;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args) {
-        System.out.println("menu:");
+
+        Scanner s = new Scanner(System.in);
+        int menuvar;
+
+        printMenu();
+
+
+        menuvar = s.nextInt();
+
+        if (menuvar==1)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+            System.out.println("YOU need to Select A Port To Create Container:");
+            System.out.println("    Your Available Ports:");
+            //TODO SHOW ALL PORTS AND COORDINATES, AVAILABLE INFO
+        }
+        else if (menuvar==2)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+            System.out.println("YOU need to Select A Port To Create Ship:");
+            System.out.println("    Your Available Ports:");
+
+        }
+        else if (menuvar==3)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+        }
+        else if (menuvar==4)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+        }
+        else if (menuvar==5)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+        }
+        else if (menuvar==6)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+        }
+        else if (menuvar==7)
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+        }
+        else
+        {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("*********************************************");
+
+            System.out.println("Invalid input.");
+
+            System.out.println("*********************************************");
+
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+        }
+    }
+
+    public static void printMenu()
+    {
+        System.out.println("*********************************************");
         System.out.println("1-) Create a container.");
         System.out.println("2-) Create a ship.");
         System.out.println("3-) Create a port.");
@@ -9,8 +104,6 @@ public class Main {
         System.out.println("5-) Unload a container from a ship.");
         System.out.println("6-) Send ship to another port.");
         System.out.println("7-) Refuel ship.");
-
-
-        System.out.println("");
+        System.out.println("*********************************************");
     }
 }
